28587b6879c1498487fc52156b8ac00c893cbf42
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-05-10 18:56:37 +0900

    간단한 테스트 코드 작성

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
+public class Application {
+    public static void main(String[] args){
+        //내장 WAS 실행, 그 덕에 항상 서버에 톰캣을 설치할 필요가 없고, 스프링 부트로 만들어진 Jar 파일로 실행하면 된다.
+        SpringApplication.run(Application.class, args);
+    }
+}

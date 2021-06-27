c7683c019ec532330b6da15d6658eac63b092fae
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-06-28 02:18:02 +0900

    기타 수정 + Execution failed for task ':test' 오류 수정

--- src/main/java/com/jojoldu/book/springboot/service/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/springboot/service/BaseTimeEntity.java
@@ -12,11 +12,14 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+//BaseTimeEntity클래스에 Auditing 기능을 포함시킵니다.
 public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdDate;
+    //Entity가 생성되어 저장될 때 시간이 자동 저장됩니다.
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+    //조회한 Entity의 값을 변경할 때 시간이 자동 저장됩니다.
 }
